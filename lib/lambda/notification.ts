40e9946f47b46ca8453e32454603e0c71a6b0d6d
Jett Gamboa <jettgamboa@gmail.com>
2023-05-31 22:07:40 +0800

    adapted to changes in customer payload

--- lib/lambda/notification.ts
+++ lib/lambda/notification.ts
@@ -6,7 +6,7 @@ function getCustomerDetail(customer) {
   const options = {
     headers: {
       'Content-type': 'application/json',
-      'Authorization': `Basic cmtfbGl2ZV81MUVMYzJuRVBEQjVQTUw1elByVXFPc2dtYjdvQUl2bUJPSnFLSjdGU25ObTVvdW1TdmpKUTBCS0lzM2luODAzV1c5QXR3QVV2Q0RtaWZNd0ptYTFzTjZ3cDAwVzQwYjlnNGo6`
+      'Authorization': `Basic APIKEY`
       }   
   };
 
@@ -41,18 +41,18 @@ async function handler(event) {
   // console.log("request:", JSON.stringify(event.Records[0].Sns.Message));
 
   const eventType = record.type;
-  const customer = record.data.object.customer;
+  const customer = record.data.object.customer_name;
   const failureMessage = record.data.object.failure_message;
 
-  const detail = await getCustomerDetail(customer);
+  // const detail = await getCustomerDetail(customer);
 
   console.log("%s: %s for %s", eventType, failureMessage, customer);
-  console.log("%s", JSON.stringify(detail));
+  // console.log("%s", JSON.stringify(detail));
 
   const params : PublishCommandInput = {
-    Message: `${eventType}: ${detail.shipping.name}\n${failureMessage}\n\n${detail}`,
+    Message: `${eventType}: ${customer}\n${failureMessage}\n`,
     TopicArn: process.env.TOPIC_ARN,
-    Subject: `Stripe Payment Failure - ${eventType}: ${detail.shipping.name}`,
+    Subject: `Stripe Payment Failure - ${eventType}: ${customer}`,
   }
 
   const sns = new SNSClient({region: process.env.AWS_REGION});
